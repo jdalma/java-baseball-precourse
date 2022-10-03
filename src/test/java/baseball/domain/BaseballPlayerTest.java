21c5c90428f6f6ff5312c9740c7de166b1208abb
정현준 <jeongdalma@gmail.com>
2022-10-03 16:34:27 +0900

    test(domain) : 플레이어 숫자 비교 테스트 코드 추가

--- src/test/java/baseball/domain/BaseballPlayerTest.java
+++ src/test/java/baseball/domain/BaseballPlayerTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("BaseballPlayer 테스트")
 class BaseballPlayerTest {
 
     private final int NUMBER_SIZE = 3;
@@ -52,6 +53,78 @@ class BaseballPlayerTest {
                 for (int i = 0; i < userNumbers.size(); i++) {
                     int number = userNumbers.get(i);
                     assertThat(number).isEqualTo(numbers.charAt(i) - 48);
+                }
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("compare()")
+    class Describe_CompareMethod{
+
+        @Nested
+        @DisplayName("호출한 플레이어는 [1,2,3]을 가지고 있다면")
+        class Context_CallMethodPlayer_Has_123{
+
+            private final BaseballPlayer PLAYER = BaseballPlayer.user("123");
+
+            @Nested
+            @DisplayName("상대 플레이어가 [1,2,4]를 가지고 있다면")
+            class Context_OtherPlayer_Has_124{
+
+                private final BaseballPlayer OTHER_PLAYER = BaseballPlayer.user("124");
+
+                @Test
+                @DisplayName("2스트라이크를 반환한다.")
+                void It_TwoStrike(){
+                    BaseballResult result = PLAYER.compare(OTHER_PLAYER);
+
+                    assertThat(result.getResultScore()).isEqualTo("2스트라이크");
+                }
+            }
+
+            @Nested
+            @DisplayName("상대 플레이어가 [3,2,1]를 가지고 있다면")
+            class Context_OtherPlayer_Has_321{
+
+                private final BaseballPlayer OTHER_PLAYER = BaseballPlayer.user("321");
+
+                @Test
+                @DisplayName("2볼 1스트라이크를 반환한다.")
+                void It_TowBallOneStrike(){
+                    BaseballResult result = PLAYER.compare(OTHER_PLAYER);
+
+                    assertThat(result.getResultScore()).isEqualTo("2볼 1스트라이크");
+                }
+            }
+
+            @Nested
+            @DisplayName("상대 플레이어가 [3,1,2]를 가지고 있다면")
+            class Context_OtherPlayer_Has_312{
+
+                private final BaseballPlayer OTHER_PLAYER = BaseballPlayer.user("312");
+
+                @Test
+                @DisplayName("3볼을 반환한다.")
+                void It_TowBallOneStrike(){
+                    BaseballResult result = PLAYER.compare(OTHER_PLAYER);
+
+                    assertThat(result.getResultScore()).isEqualTo("3볼");
+                }
+            }
+
+            @Nested
+            @DisplayName("상대 플레이어가 [7,8,9]를 가지고 있다면")
+            class Context_OtherPlayer_Has_789{
+
+                private final BaseballPlayer OTHER_PLAYER = BaseballPlayer.user("789");
+
+                @Test
+                @DisplayName("낫싱을 반환한다.")
+                void It_TowBallOneStrike(){
+                    BaseballResult result = PLAYER.compare(OTHER_PLAYER);
+
+                    assertThat(result.getResultScore()).isEqualTo("낫싱");
                 }
             }
         }
