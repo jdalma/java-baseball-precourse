f04d30b5c377f2335d2e7aae2484693a1da5fab8
정현준 <jeongdalma@gmail.com>
2022-10-03 16:54:28 +0900

    refactor(view) : 게임 메시지 출력 책임 이동

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -30,7 +30,7 @@ public class BaseballGame implements GameController {
             result = play(computer);
             baseballView.println(result.getResultScore());
         } while (!result.isGameOver());
-        baseballView.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        baseballView.continueGame();
     }
 
     private BaseballResult play(BaseballPlayer computer) {
--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -3,6 +3,7 @@ package baseball.view;
 public class BaseballView implements GameView {
 
     private final String START_MESSAGE = "숫자를 입력해 주세요 : ";
+    private final String GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private final String CONTINUE_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     @Override
@@ -12,6 +13,7 @@ public class BaseballView implements GameView {
 
     @Override
     public void continueGame() {
+        System.out.println(GAME_OVER);
         System.out.println(CONTINUE_GAME);
     }
 
